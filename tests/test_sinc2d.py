6c563f80cc810cdd119a79b35becc07a75ebb027
Igors Dubanevics <26872977+igorsdub@users.noreply.github.com>
2025-07-10 11:00:54 +0900

    Add a corner case

--- tests/test_sinc2d.py
+++ tests/test_sinc2d.py
@@ -16,4 +16,9 @@ def test_edge_y():
     exp = (2.0 / np.pi)
     obs = sinc2d(np.pi / 2.0, 0.0)
     assert obs == exp
+
+def test_corner():
+    exp = 1.0
+    obs = sinc2d(0.0, 0.0)
+    assert obs == exp
     

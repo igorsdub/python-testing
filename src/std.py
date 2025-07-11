b291db5f3e83c37b4af2bfaa942f60e2473ba955
Igors Dubanevics <26872977+igorsdub@users.noreply.github.com>
2025-07-11 10:56:15 +0900

    Fix standard deviation funciotn to pass all tests

--- src/std.py
+++ src/std.py
@@ -1,5 +1,17 @@
 def std(vals):
-    # a little better
+    """Standard deviation of a list of values.
+
+    Args:
+        vals (float): A list of values to calculate the standard deviation for.
+
+    Returns:
+        float: The standard deviation of the values in the list.
+    """
     if len(vals) == 0: # Special case the empty list.
         return 0.0
-    return vals[-1] / 2.0 # By being clever, we can get away without doing real work.
+    else:
+        size_ = len(vals)
+        mean  = sum(vals) / size_
+        diff_squared = [(x - mean) ** 2 for x in vals]
+        variance = sum(diff_squared) / size_
+        return variance ** 0.5
